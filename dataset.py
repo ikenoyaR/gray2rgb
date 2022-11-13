fc7925913210ba18fa9cada283b688b4cd95d94a
reita0625@gmail.com <reita0625@gmail.com>
2022-11-13 15:01:39 +0900

    Update

--- dataset.py
+++ dataset.py
@@ -4,10 +4,30 @@ import argparse
 
 
 
-from PIL import Image
 import numpy as np
+import glob
 import cv2
 import torch
 import torch.nn as nn
 import torchvision
+from torch.utils.data import Dataset
+
+
+class Sun_dataset(Dataset):
+    def __init__(self, data_path, transform=None, target_transform=None):
+        self.data_path = data_path
+        self.img_path_list = glob.glob(os.path.join(data_path, '*', '*.jpg'))
+        self.transform = transform
+        self.target_transform = target_transform
+    
+    def __len__(self):
+        return len(self.img_path_list)
+    
+    def __getitem__(self, idx):
+        img_path = self.img_path_list[idx]
+        image = cv2.imread(img_path)
+        target_image = image
+
+    def img_processing(img):
+        pass
 
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='inference/images', help='source')
     parser.add_argument('--img-size', type=str, default='640x640', help='inference size (pixels) "height x width"')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
     parser.add_argument('--project', default='runs/RGB_image', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--mode', default='train', choices=['train', 'test', 'predict'], help='choose modes train or test or predict')
+    parser.add_argument('--mode', default='test', choices=['train', 'test', 'predict'], help='choose modes train or test or predict')
     args = parser.parse_args()
     print(args)
     main(args)
