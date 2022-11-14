dba3b1af36ea9c0d9f022fe19066321e918c3967
reita0625@gmail.com <reita0625@gmail.com>
2022-11-15 01:04:08 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -17,9 +17,15 @@ class Sun_dataset(Dataset):
     def __init__(self, data_path, train = True, transform=None, target_transform=None):
         self.data_path = data_path
         self.train = train
-        self.img_path_list = glob.glob(os.path.join(data_path, '*', '*', '*.jpg'))
-        self.transform = transform
-        self.target_transform = target_transform
+        if self.train:
+            self.img_path_list = glob.glob(os.path.join(data_path, '*', '*', '*.jpg'))
+            self.transform = transform
+            self.target_transform = target_transform
+        else:
+            self.img_path_list = glob.glob(os.path.join(data_path, '*.jpg'))
+            self.transform = transform
+            self.target_transform = target_transform
+
     
     def __len__(self):
         return len(self.img_path_list)
@@ -27,15 +33,22 @@ class Sun_dataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.img_path_list[idx]
         image = cv2.imread(img_path)
-        target_image = self.img_processing(image)
-        gray_image = self.RGB2GRAY(target_image)
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            target_image = self.target_transform(target_image)
-        return gray_image, target_image
+        if self.train:
+            target_image = self.img_processing(image)
+            gray_image = self.RGB2GRAY(target_image)
+            if self.transform:
+                gray_image = self.transform(gray_image)
+            if self.target_transform:
+                target_image = self.target_transform(target_image)
+            return gray_image, target_image
 
-    def img_processing(img):
+        else:
+            gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            if self.transform:
+                gray_image = self.transform(gray_image)
+            return gray_image
+
+    def img_processing(img): # Data augumentation will be added.
         return img[:, :, ::-1]
 
     def RGB2GRAY(img):
